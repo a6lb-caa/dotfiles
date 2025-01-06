8088df976092b16febdd775dd8ccbbd8afc20f2f
a6lb-caa <abdul.taleb@caa.com>
2025-01-06 05:06:33 +0000

    fix: neovim and remove vim

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,23 +20,17 @@ ln -s ~/.local/squashfs-root/AppRun ~/.local/bin/nvim
 # Bring in our custom neovim config
 echo "XDG_CONFIG_HOME=$HOME" >> ~/.profile
 mkdir ~/.config/nvim
-cp -R nvim/.config/nvim/* ~/.config/nvim/
+cp -r ~/dotfiles/nvim/.config/nvim/* ~/.config/nvim
 
 # Get Neovim mostly ready to go
 cd /workspaces/$RepositoryName
 # poetry run pip install pynvim ipython  # Avoid if we can help it
 poetry run nvim --headless +":UpdateRemotePlugins" +"q!"
 
-# Go ahead and configure vim as well while we're at it
-git clone https://github.com/shea-parkes/vim-config ~/.vim
-cd ~/.vim
-git submodule init
-git submodule update
-
 # Configure zsh as default
 sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
-cp /zsh/* ~/
-cp /zimfw/* ~/
+cp ~/dotfiles/zsh/* ~/
+cp ~/dotfiles/zimfw/* ~/
 
 # Setup git completions for bash
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
